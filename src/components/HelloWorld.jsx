923c4b1e905b3789a41e255d193504d2df6cc70c
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2022-06-20 17:00:16 -0500

    First commit

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function HelloWorld() {
+  return (
+    <>
+      <div>Hello World!</div>
+      <a href='' target='_blank'>Link to web site</a>
+    </>
+  );
+}
